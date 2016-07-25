2e7f0e40175cf767549061f2c8bfd39c3ed3c13a
Vickenty Fesunov <kent@setattr.net>
2016-07-25 21:02:23 +0200

    Use Data.Generics to traverse the tree.

--- src/Language/C/Transform/HoistVariables.hs
+++ src/Language/C/Transform/HoistVariables.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 -- |
 -- Module: Language.C.Transform.HoistVariables
 -- Description: Move variable declarations to the top of the function
@@ -9,8 +10,9 @@
 
 module Language.C.Transform.HoistVariables (hoistVariables) where
 import Data.Maybe
+import Data.Generics
 import Language.C
-import Control.Monad.State.Lazy
+import Control.Monad.State.Strict
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 
@@ -115,16 +117,17 @@ scope env = do
 -- >   }
 -- > }
 hoistVariables :: CFunDef -> [String] -> CFunDef
-hoistVariables (CFunDef declspec declr decls (CCompound [] items _) _) globals =
+hoistVariables (CFunDef declspec declr decls body _) globals =
   let args = funDeclrNames declr
       env = initEnv (globals ++ args)
-      (items', env') = runState (concatM $ mapM processItem items) env
+      (body', env') = runState (everywhereM' process body) env
       declstmt = map CBlockDecl (toDeclare env')
-      body' = CCompound [] (declstmt ++ items') undefNode
   in
-    CFunDef declspec declr decls body' undefNode
-  where concatM = liftM concat
-hoistVariables _ _ = error "unsupported function definition structure"
+    CFunDef declspec declr decls (prependItems body' declstmt) undefNode
+
+prependItems :: CStat -> [CBlockItem] -> CStat
+prependItems (CCompound labels items ni) more = (CCompound labels (more ++ items) ni)
+prependItems _ _ = error "can not prepend items to non compound statement"
 
 funDeclrNames :: CDeclr -> [String]
 funDeclrNames (CDeclr mident derived _ _ _) =
@@ -139,60 +142,31 @@ derivedDeclrName (CFunDeclr (Right (decls, _)) _ _) =
   map identToString $ concat $ map (maybeToList . declToIdent) decls
 derivedDeclrName _ = []
 
-processItem :: CBlockItem -> Env [CBlockItem]
+process :: Typeable a => a -> Env a
+process = return `extM` processItem `extM` processStmt `extM` processExpr
+
+processItem :: CBlockItem -> Env CBlockItem
 processItem (CBlockDecl decl) = do
   started <- gets codeBegan
   if started
     then do
     initlist <- hoistDecl decl
     return $ case initlist of
-      [] -> []
-      [ one ] -> [CBlockStmt (CExpr (Just one) undefNode)]
-      many -> [CBlockStmt (CExpr (Just (CComma many undefNode)) undefNode)]
+      [] -> CBlockStmt (CExpr Nothing undefNode)
+      [ one ] -> CBlockStmt (CExpr (Just one) undefNode)
+      many -> CBlockStmt (CExpr (Just (CComma many undefNode)) undefNode)
     else do
     _ <- addDecl decl False
-    return []
+    return $ CBlockStmt (CExpr Nothing undefNode)
 processItem (CBlockStmt stmt) = do
   modify $ \st -> st { codeBegan = True }
-  stmt' <- processStmt stmt
-  return $ [ CBlockStmt stmt' ]
+  return $ CBlockStmt stmt
 processItem (CNestedFunDef _) = error "nested functions are not supported"
 
 processStmt :: CStat -> Env CStat
-processStmt (CLabel label stmt attr ni) = do
-  stmt' <- processStmt stmt
-  return $ CLabel label stmt' attr ni
-processStmt (CCase expr stmt ni) = do
-  expr' <- processExpr expr
-  stmt' <- processStmt stmt
-  return $ CCase expr' stmt' ni
-processStmt (CCases lower upper stmt ni) = do
-  lower' <- processExpr lower
-  upper' <- processExpr upper
-  stmt' <- processStmt stmt
-  return $ CCases lower' upper' stmt' ni
-processStmt (CDefault stmt ni) = do
-  stmt' <- processStmt stmt
-  return $ CDefault stmt' ni
-processStmt (CExpr mexpr ni) = do
-  expr' <- mapM processExpr mexpr
-  return $ CExpr expr' ni
 processStmt (CCompound labels items ni) = do
-  items' <- scope (mapM processItem items)
-  return $ CCompound labels (concat items') ni
-processStmt (CIf expr thenstmt melsestmt ni) = do
-  expr' <- processExpr expr
-  thenstmt' <- processStmt thenstmt
-  melsestmt' <- mapM processStmt melsestmt
-  return $ CIf expr' thenstmt' melsestmt' ni
-processStmt (CSwitch expr stmt ni) = do
-  expr' <- processExpr expr
-  stmt' <- processStmt stmt
-  return $ CSwitch expr' stmt' ni
-processStmt (CWhile expr stmt isDo ni) = do
-  expr' <- processExpr expr
-  stmt' <- processStmt stmt
-  return $ CWhile expr' stmt' isDo ni
+  items' <- scope (everywhereM' process items)
+  return $ CCompound labels (filter (not . emptyExprStmt) items') ni
 processStmt (CFor first cond step stmt ni) = do
   first' <- case first of
     (Right decl) -> do
@@ -200,83 +174,23 @@ processStmt (CFor first cond step stmt ni) = do
       return $ case exprs of
         [] -> Left Nothing
         xs -> Left (Just (CComma xs undefNode))
-    (Left mexpr) -> do
-      mexpr' <- mapM processExpr mexpr
-      return $ Left mexpr'
-  cond' <- mapM processExpr cond
-  step' <- mapM processExpr step
-  stmt' <- processStmt stmt
-  return $ CFor first' cond' step' stmt' ni
-processStmt stmt@(CGoto _ _) = return stmt
-processStmt (CGotoPtr _ _) = error "goto expression is not supported"
-processStmt stmt@(CCont _) = return stmt
-processStmt stmt@(CBreak _) = return stmt
-processStmt (CReturn mexpr ni) = do
-  mexpr' <- mapM processExpr mexpr
-  return $ CReturn mexpr' ni
-processStmt (CAsm _ _) = error "inline assembly is not supported"
+    expr -> return expr
+  return $ CFor first' cond step stmt ni
+processStmt s = return s
 
 processExpr :: CExpr -> Env CExpr
-processExpr (CComma exprs ni) = do
-  exprs' <- mapM processExpr exprs
-  return $ CComma exprs' ni
-processExpr (CAssign op lexpr rexpr ni) = do
-  lexpr' <- processExpr lexpr
-  rexpr' <- processExpr rexpr
-  return $ CAssign op lexpr' rexpr' ni
-processExpr (CCond ex1 ex2 ex3 ni) = do
-  ex1' <- processExpr ex1
-  ex2' <- mapM processExpr ex2
-  ex3' <- processExpr ex3
-  return $ CCond ex1' ex2' ex3' ni
-processExpr (CBinary op ex1 ex2 ni) = do
-  ex1' <- processExpr ex1
-  ex2' <- processExpr ex2
-  return $ CBinary op ex1' ex2' ni
-processExpr (CCast decl ex ni) = do
-  ex' <- processExpr ex
-  return $ CCast decl ex' ni
-processExpr (CUnary op ex ni) = do
-  ex' <- processExpr ex
-  return $ CUnary op ex' ni
-processExpr (CSizeofExpr ex ni) = do
-  ex' <- processExpr ex
-  return $ CSizeofExpr ex' ni
-processExpr e@(CSizeofType _ _) = return e
-processExpr (CAlignofExpr ex ni) = do
-  ex' <- processExpr ex
-  return $ CAlignofExpr ex' ni
-processExpr e@(CAlignofType _ _) = return e
-processExpr (CComplexReal ex ni) = do
-  ex' <- processExpr ex
-  return $ CComplexReal ex' ni
-processExpr (CComplexImag ex ni) = do
-  ex' <- processExpr ex
-  return $ CComplexImag ex' ni
-processExpr (CIndex ex1 ex2 ni) = do
-  ex1' <- processExpr ex1
-  ex2' <- processExpr ex2
-  return $ CIndex ex1' ex2' ni
-processExpr (CCall ex1 ex2 ni) = do
-  ex1' <- processExpr ex1
-  ex2' <- mapM processExpr ex2
-  return $ CCall ex1' ex2' ni
-processExpr (CMember ex ident flag ni) = do
-  ex' <- processExpr ex
-  return $ CMember ex' ident flag ni
 processExpr (CVar ident ni) = do
   namemap <- gets renameMap
   let ident' = case Map.lookup (identToString ident) namemap of
         Nothing -> ident
         Just name -> internalIdent name
   return $ CVar ident' ni
-processExpr e@(CConst _) = return e
-processExpr e@(CCompoundLit _ _ _) = return e
-processExpr (CStatExpr stmt ni) = do
-  stmt' <- processStmt stmt
-  return $ CStatExpr stmt' ni
-processExpr (CLabAddrExpr _ _) = error "label address expression is not supported"
-processExpr e@(CBuiltinExpr _) = return e
+processExpr e = return e
+
+-- Return true if statement contains empty expression.
+emptyExprStmt :: CBlockItem -> Bool
+emptyExprStmt (CBlockStmt (CExpr Nothing _)) = True
+emptyExprStmt _ = False
 
 -- hoist declarations, returing a list of initializer expressions.
 hoistDecl :: CDecl -> Env [CExpr]
@@ -288,7 +202,12 @@ hoistDecl (CDecl spec declrlist _) = do
 declrToExpr :: (String, (Maybe CDeclr, Maybe CInit, Maybe CExpr)) -> Env [CExpr]
 declrToExpr (_, (_, Nothing, Nothing)) = return []
 declrToExpr (name, (Just (CDeclr _ _ _ _ _), Just (CInitExpr expr _), Nothing)) = do
-  expr' <- processExpr expr
   let ident = internalIdent name
-  return [CAssign CAssignOp (CVar ident undefNode) expr' undefNode]
+  return [CAssign CAssignOp (CVar ident undefNode) expr undefNode]
 declrToExpr _ = error "unsupported initializer"
+
+-- top-down variant of everywhereM, similar to everywhere'
+everywhereM' :: Monad m => GenericM m -> GenericM m
+everywhereM' f x = do
+  x' <- f x
+  gmapM (everywhereM' f) x'
